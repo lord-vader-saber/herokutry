19dda82cbfe0ebd3d669fc15d6cfacbfec2be9e5
Lord Vader <72856051+lord-vader-saber@users.noreply.github.com>
2021-06-27 02:18:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname,'/dist/covid-app'));
 app.get('/*',function(req,res){res.sendFile(path.join(__dirname +'dist/covid-app/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
