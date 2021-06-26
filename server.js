e61f1aec9a164283fb0a3834f780a6a9add708a6
Lord Vader <72856051+lord-vader-saber@users.noreply.github.com>
2021-06-27 02:07:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const path = require("path");
 const app = express();
 app.use(express.static(CovidApp,'/dist/covid-app'));
 app.get('/*',function(req,res){res.sendFile(path.join(CovidApp+'dist/covid-app/index.html'));});
-app.listen(process.env.PORT||8080);
+app.listen(process.env.PORT || 8080);
